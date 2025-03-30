c27074db5b88a3483c6befa7e04af2dcec9208cc
lancast <angelprep92@gmail.com>
2025-03-29 21:20:11 -0600

    fix: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 export const routes: Routes = [
-  { path: 'cv', component: ProfileComponent }, // Agrega la ruta
-  { path: '', redirectTo: '/cv', pathMatch: 'full' }, // Redirige la ruta raiz a /cv
+  { path: '', component: ProfileComponent }, // Agrega la ruta
 ];
