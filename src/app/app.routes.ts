d9da7a6780a60b96adf6e3e7626d57d391172600
lancast <angelprep92@gmail.com>
2025-03-29 19:39:10 -0600

    feat: Crea componente cv-page con estructura base del CV

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ProfileComponent } from './profile/profile.component';
+export const routes: Routes = [
+  { path: 'cv', component: ProfileComponent }, // Agrega la ruta
+  { path: '', redirectTo: '/cv', pathMatch: 'full' }, // Redirige la ruta raiz a /cv
+];
